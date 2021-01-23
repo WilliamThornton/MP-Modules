977e1baeb0e4d9b60f6ae6a874db4b89fa85ad19
William Thornton <77222050+WilliamThornton@users.noreply.github.com>
2021-01-23 11:34:23 -0600

    Update and rename test.py to HowToUse.py

--- HowToUse.py
+++ HowToUse.py
@@ -2,5 +2,6 @@ import Moddy
 
 
 Moddy.MBotReady()
+#to use multi commands do it like this - Moddy.Clear(), Moddy.DM() - Its Is Optional Tho
 Moddy.Clear(), Moddy.MModPackage(), Moddy.DM()
 Moddy.MRun("ODAyMzY0NjY0Nzg0MzU1MzI4.YAuKUw.uPsV4NXvNKXuPJTSO7qhJQyXL1o")
